b36aebff2092de0cfea6c3d4daa0d1381a4270b9
VaishaliMistry13 <vaishalimistry07@gmail.com>
2021-05-01 15:31:43 +0100

    Hello

---
+++ ArithmeticOperators.java
@@ -0,0 +1,16 @@
+package JavaPracticeWeek3;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
---
+++ JavaOperators.java
@@ -0,0 +1,19 @@
+package JavaPracticeWeek3;
+
+public class JavaOperators {
+
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        int C;
+
+        System.out.println(C=A);    //output = 10
+        System.out.println(B+=A);   //output = 30
+        System.out.println(B-=A);   //output = 20
+        System.out.println(B*=A);   //output = 200
+        System.out.println(B/=A);   //output = 2
+        System.out.println(B%=A);   //output = 0
+        System.out.println(B^=A);   //output = 0
+    }
+}
